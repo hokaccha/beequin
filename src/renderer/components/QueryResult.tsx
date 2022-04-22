0868f756331315eb093fe43da572819dd6e5871d
Kazuhito Hokamura <k.hokamura@gmail.com>
2022-04-22 22:56:52 +0900

    Table view

--- src/renderer/components/QueryResult.tsx
+++ src/renderer/components/QueryResult.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 import type { FC } from "react";
+import type { JobResult } from "~/../main/bigquery/client";
 import type { QueryState } from "~/lib/query";
 
 type Props = {
@@ -8,5 +9,50 @@ type Props = {
 export const QueryResult: FC<Props> = ({ queryState }) => {
   if (queryState === null) return null;
 
-  return <Box>{<pre>{JSON.stringify(queryState, null, 2)}</pre>}</Box>;
+  switch (queryState.status) {
+    case "running": {
+      return <div>...</div>;
+    }
+    case "canceled": {
+      return <Box>{<pre>{JSON.stringify(queryState, null, 2)}</pre>}</Box>;
+    }
+    case "error": {
+      return <Box>{<pre>{JSON.stringify(queryState, null, 2)}</pre>}</Box>;
+    }
+    case "completed": {
+      return <QueryResultTable result={queryState.result} />;
+    }
+    default: {
+      throw new Error("Invalid state");
+    }
+  }
+};
+
+const QueryResultTable: FC<{ result: JobResult }> = ({ result }) => {
+  const rows = result.responseRows[0];
+  const headers = Object.keys(rows[0]);
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          {headers.map((h: any, i: number) => (
+            <Th key={i} borderColor="gray.500" textTransform="none" fontSize={14}>
+              {h}
+            </Th>
+          ))}
+        </Tr>
+      </Thead>
+      <Tbody>
+        {rows.map((row: any, j: number) => (
+          <Tr key={j}>
+            {Object.values(row).map((val, k) => (
+              <Td key={k}>
+                <pre>{JSON.stringify(val, null, 2)}</pre>
+              </Td>
+            ))}
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
 };
